68b980252a602e2c5f400383fb5c9e9b468c58ed
Vicent Marti <tanoku@gmail.com>
2012-10-02 12:56:29 +0200

    Work in progress lol

--- msgpacker.c
+++ msgpacker.c
@@ -3,309 +3,333 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
-TODO: make these noop if __BIG_ENDIAN__
+static inline void swap16(unsigned char *buffer, void *out)
+{
+    unsigned char *risky = (unsigned char *)out;
+    ptr[0] = buffer[1];
+    ptr[1] = buffer[0];
+}
 
-#ifdef __BIG_ENDIAN__
-#define msgpacker_swap16(b, l, o)
-#define msgpacker_swap32(b, l, o)
-#define msgpacker_swap64(b, l, o)
+static inline void swap32(unsigned char *buffer, void *out)
+{
+    unsigned char *ptr = (unsigned char *)out;
+    ptr[0] = buffer[3];
+    ptr[1] = buffer[2];
+    ptr[2] = buffer[1];
+    ptr[3] = buffer[0];
+}
+
+static inline void swap64(unsigned char *buffer, void *out)
+{
+	unsigned char *ptr = (unsigned char *)out;
+	ptr[0] = buffer[7];
+	ptr[1] = buffer[6];
+	ptr[2] = buffer[5];
+	ptr[3] = buffer[4];
+	ptr[4] = buffer[3];
+	ptr[5] = buffer[2];
+	ptr[6] = buffer[1];
+	ptr[7] = buffer[0];
+}
+
+typedef void * mo_value;
+
+enum mo_integer_t {
+	MSGPACK_UINT_8,
+	MSGPACK_UINT_16,
+	MSGPACK_UINT_32,
+	MSGPACK_UINT_64,
+
+	MSGPACK_INT_8,
+	MSGPACK_INT_16,
+	MSGPACK_INT_32,
+	MSGPACK_INT_64
+};
+
+enum mo_atom_t {
+	MSGPACK_NIL,
+	MSGPACK_TRUE,
+	MSGPACK_FALSE
+};
+
+#ifdef MOCHILO_PACKED_INTEGERS
+	typedef union {
+		uint8_t uint8;
+		uint16_t uint16;
+		uint32_t uint32;
+		uint64_t uint64;
+
+		int8_t int8;
+		int16_t int16;
+		int32_t int32;
+		int64_t int64;
+	} mo_integer;
 #else
-*/
-static inline void msgpacker_swap16(unsigned char *buffer, size_t len, void *out) {
-  if (len > 1) {
-    unsigned char *risky = (unsigned char *)out;
-    risky[0] = buffer[1];
-    risky[1] = buffer[0];
-  }
+	typedef uint64_t mo_integer;
+#endif
+
+struct mochilo_callbacks {
+	mo_value (*array_new)(size_t array_size, void *opaque);
+	void (*array_append)(mo_value array, mo_value element, void *opaque);
+
+	mo_value (*hash_new)(void *opaque);
+	void (*hash_set)(mo_value hash, mo_value key, mo_value val, void *opaque);
+
+	mo_value (*bytes_new)(const char *bytes, size_t len, void *opaque);
+
+	mo_value (*int_new)(const mo_integer *value, mo_integer_t int_type, void *opaque);
+	mo_value (*atom_new)(mo_atom_t type, void *opaque);
+
+	mo_value (*float_new)(float value, void *opaque);
+	mo_value (*double_new)(double value, void *opaque);
+};
+
+enum msgpack_t {
+	MSGPACK_T_NIL = 0xc0,
+	MSGPACK_T_FALSE = 0xc2,
+	MSGPACK_T_TRUE = 0xc3,
+	MSGPACK_T_FLOAT = 0xca,
+	MSGPACK_T_DOUBLE = 0xcb,
+	MSGPACK_T_UINT8	= 0xcc,
+	MSGPACK_T_UINT16 = 0xcd,
+	MSGPACK_T_UINT32 = 0xce,
+	MSGPACK_T_UINT64 = 0xcf,
+	MSGPACK_T_INT8 = 0xd0,
+	MSGPACK_T_INT16	 = 0xd1,
+	MSGPACK_T_INT32 = 0xd2,
+	MSGPACK_T_INT64	 = 0xd3,
+	MSGPACK_T_RAW16	= 0xda,
+	MSGPACK_T_RAW32	 = 0xdb,
+	MSGPACK_T_ARRAY16 = 0xdc,
+	MSGPACK_T_ARRAY32 = 0xdd,
+	MSGPACK_T_MAP16	= 0xde,
+	MSGPACK_T_MAP32	= 0xdf
+};
+
+static struct mochilo_callbacks MOCHILO_CB;
+
+static inline int unpack_array(mo_value *_array, size_t elements, struct mochilo_buf *buf)
+{
+	size_t i;
+	int error;
+
+	mo_value array = MOCHILO_CB.array_new(elements, opaque);
+
+	for (i = 0; i < elements; ++i) {
+		mo_value element;
+
+		if ((error = mochilo_unpack_one(&element, buf)) < 0)
+			return error;
+
+		MOCHILO_CB.array_append(array, element, opaque);
+	}
+
+	*_array = array;
+	return 0;
 }
 
-static inline void msgpacker_swap32(unsigned char *buffer, size_t len, void *out) {
-  if (len > 3) {
-    unsigned char *risky = (unsigned char *)out;
-    risky[0] = buffer[3];
-    risky[1] = buffer[2];
-    risky[2] = buffer[1];
-    risky[3] = buffer[0];
-  }
+static inline int unpack_hash(mo_value *_hash, size_t elements, struct mochilo *state)
+{
+	size_t i;
+	int error;
+
+	mo_value hash = MOCHILO_CB.hash_new(opaque);
+
+	for (i = 0; i < elements; ++i) {
+		mo_value key, value;
+
+		if ((error = mochilo_unpack_one(&key, buf)) < 0)
+			return error;
+
+		if ((error = mochilo_unpack_one(&value, buf)) < 0)
+			return error;
+
+		MOCHILO_CB.hash_set(hash, key, value, opaque);
+	}
+
+	*_hash = hash;
+	return 0;
 }
 
-static inline void msgpacker_swap64(unsigned char *buffer, size_t len, void *out) {
-  if (len > 7) {
-    unsigned char *risky = (unsigned char *)out;
-    risky[0] = buffer[7];
-    risky[1] = buffer[6];
-    risky[2] = buffer[5];
-    risky[3] = buffer[4];
-    risky[4] = buffer[3];
-    risky[5] = buffer[2];
-    risky[6] = buffer[1];
-    risky[7] = buffer[0];
-  }
-}
-/* #endif */
-
-static inline void msgpacker__decode_uint8(unsigned char *buffer, size_t len, uint8_t *out) {
-  if (len > 0) {
-    unsigned char *risky = (unsigned char *)out;
-    risky[0] = buffer[0];
-  }
+void mochilo_init(const struct mochilo_callbacks *callbacks)
+{
+	memcpy(&MOCHILO_CB, callbacks, sizeof(struct mochilo_callbacks));
 }
 
-static inline void msgpacker__decode_int8(unsigned char *buffer, size_t len, int8_t *out) {
-  if (len > 0) {
-    unsigned char *risky = (unsigned char *)out;
-    risky[0] = buffer[0];
-  }
+int mochilo_unpack_one(mo_value *_value, struct mochilo_buf *buf)
+{
+	uint8_t leader;
+	mo_integer integer;
+
+#define WARD_BYTES(n) \
+	if (buf->ptr + n >= buf->end) { return -1; }
+
+	WARD_BYTES(1);
+	leader = *(buf->ptr)++;
+
+	switch (leader) {
+		case MSGPACK_T_UINT8:
+		case MSGPACK_T_INT8:
+			WARD_BYTES(1);
+			integer.uint8 = *buf->ptr++;
+			*_value = MOCHILO_CB.int_new(&integer, leader, opaque);
+			break;
+
+		case MSGPACK_T_UINT16:
+		case MSGPACK_T_INT16:
+			WARD_BYTES(2);
+			swap16(buf->ptr, &integer.uint16);
+			buf->ptr += 2;
+
+			*_value = MOCHILO_CB.int_new(&integer, leader, opaque);
+			break;
+
+		case MSGPACK_T_UINT32:
+		case MSGPACK_T_INT32:
+			WARD_BYTES(4);
+			swap32(buf->ptr, &integer.uint32);
+			buf->ptr += 4;
+
+			*_value = MOCHILO_CB.int_new(&integer, leader, opaque);
+			break;
+
+		case MSGPACK_T_UINT64:
+		case MSGPACK_T_INT64:
+			WARD_BYTES(8);
+			swap64(buf->ptr, &integer.uint64);
+			buf->ptr += 8;
+
+			*_value = MOCHILO_CB.int_new(&integer, leader, opaque);
+			break;
+
+		case MSGPACK_T_NIL:
+		case MSGPACK_T_TRUE:
+		case MSGPACK_T_FALSE:
+			*_value = MOCHILO_CB.atom_new(leader, opaque);
+			break;
+
+		case MSGPACK_T_FLOAT:
+		{
+			float f;
+
+			WARD_BYTES(4);
+			swap32(buf->ptr, &f);
+			buf->ptr += 4;
+
+			*_value = MOCHILO_CB.float_new(f, opaque);
+			break;
+		}
+
+		case MSGPACK_T_FLOAT:
+		{
+			double d;
+
+			WARD_BYTES(8);
+			swap64(buf->ptr, &d);
+			buf->ptr += 8;
+
+			*_value = MOCHILO_CB.double_new(d, opaque);
+			break;
+		}
+
+		case MSGPACK_T_ARRAY16:
+		{
+			uint16_t length;
+
+			WARD_BYTES(2);
+			swap16(buf->ptr, &length);
+			buf->ptr += 2;
+
+			return unpack_array(_value, (size_t)length, buf);
+		}
+
+		case MSGPACK_T_ARRAY32:
+		{
+			uint32_t length;
+
+			WARD_BYTES(4);
+			swap32(buf->ptr, &length);
+			buf->ptr += 4;
+
+			return unpack_array(_value, (size_t)length, buf);
+		}
+
+		case MSGPACK_T_MAP16:
+		{
+			uint16_t length;
+
+			WARD_BYTES(2);
+			swap16(buf->ptr, &length);
+			buf->ptr += 2;
+
+			return unpack_hash(_value, (size_t)length, buf);
+		}
+
+		case MSGPACK_T_MAP32:
+		{
+			uint32_t length;
+
+			WARD_BYTES(4);
+			swap32(buf->ptr, &length);
+			buf->ptr += 4;
+
+			return unpack_hash(_value, (size_t)length, buf);
+		}
+
+		case MSGPACK_T_RAW16:
+		{
+			uint16_t length;
+
+			WARD_BYTES(2);
+			swap16(buf->ptr, &length);
+			buf->ptr += 2;
+
+			*_value = MOCHILO_CB.bytes_new(buf->ptr, (size_t)length, opaque);
+			break;
+		}
+
+		case MSGPACK_T_RAW32:
+		{
+			uint32_t length;
+
+			WARD_BYTES(4);
+			swap32(buf->ptr, &length);
+			buf->ptr += 4;
+
+			*_value = MOCHILO_CB.bytes_new(buf->ptr, (size_t)length, opaque);
+			break;
+		}
+
+		default:
+			return -1;
+	}
+
+#undef WARD_BYTES
 }
 
-static inline void msgpacker__decode_uint16(unsigned char *buffer, size_t len, uint16_t *out) {
-  msgpacker_swap16(buffer, len, out);
-}
-
-static inline void msgpacker__decode_int16(unsigned char *buffer, size_t len, int16_t *out) {
-  msgpacker_swap16(buffer, len, out);
-}
-
-static inline void msgpacker__decode_uint32(unsigned char *buffer, size_t len, uint32_t *out) {
-  msgpacker_swap32(buffer, len, out);
-}
-
-static inline void msgpacker__decode_int32(unsigned char *buffer, size_t len, int32_t *out) {
-  msgpacker_swap32(buffer, len, out);
-}
-
-static inline void msgpacker__decode_uint64(unsigned char *buffer, size_t len, uint64_t *out) {
-  msgpacker_swap64(buffer, len, out);
-}
-
-static inline void msgpacker__decode_int64(unsigned char *buffer, size_t len, int64_t *out) {
-  msgpacker_swap64(buffer, len, out);
-}
-
-static inline void msgpacker__decode_float(unsigned char *buffer, size_t len, float *out) {
-  msgpacker_swap32(buffer, len, out);
-}
-
-static inline void msgpacker__decode_double(unsigned char *buffer, size_t len, double *out) {
-  msgpacker_swap64(buffer, len, out);
-}
-
-static inline void msgpacker__decode_raw16(unsigned char *buffer, size_t len, unsigned char *out, uint16_t out_len) {
-  if (len >= (size_t)out_len) memcpy(out, buffer, out_len);
-}
-
-static inline void msgpacker__decode_raw32(unsigned char *buffer, size_t len, unsigned char *out, uint32_t out_len) {
-  if (len >= (size_t)out_len) memcpy(out, buffer, out_len);
-}
-
-void msgpacker_decode(unsigned char *buffer, size_t len) {
-  uint8_t leader;
-  size_t len_left;
-
-  uint8_t uint8_out;
-  uint16_t uint16_out;
-  uint32_t uint32_out;
-  uint64_t uint64_out;
-  int8_t int8_out;
-  int16_t int16_out;
-  int32_t int32_out;
-  int64_t int64_out;
-  float float_out;
-  double double_out;
-  unsigned char *buffer_out;
-
-  len_left = len;
-
-  leader = buffer[0];
-
-  while (len_left > 0) {
-    len_left--;
-    buffer++;
-
-    switch(leader) {
-      case 0xcc:
-        msgpacker__decode_uint8(buffer, len_left, &uint8_out);
-        len_left--;
-        printf("Decoded an uint8: %u\n", uint8_out);
-        break;
-      case 0xcd:
-        msgpacker__decode_uint16(buffer, len_left, &uint16_out);
-        len_left-=2;
-        printf("Decoded an uint16_out: %u\n", uint16_out);
-        break;
-      case 0xce:
-        msgpacker__decode_uint32(buffer, len_left, &uint32_out);
-        len_left-=4;
-        printf("Decoded an uint32_out: %u\n", uint32_out);
-        break;
-      case 0xcf:
-        msgpacker__decode_uint64(buffer, len_left, &uint64_out);
-        len_left-=8;
-        printf("Decoded an uint64_out: %llu\n", uint64_out);
-      case 0xd0:
-        msgpacker__decode_int8(buffer, len_left, &int8_out);
-        len_left--;
-        printf("Decoded an int8: %d\n", int8_out);
-        break;
-      case 0xd1:
-        msgpacker__decode_int16(buffer, len_left, &int16_out);
-        len_left-=2;
-        printf("Decoded an int16: %d\n", int16_out);
-        break;
-      case 0xd2:
-        msgpacker__decode_int32(buffer, len_left, &int32_out);
-        len_left-=4;
-        printf("Decoded an int32_out: %d\n", int32_out);
-        break;
-      case 0xd3:
-        msgpacker__decode_int64(buffer, len_left, &int64_out);
-        len_left-=8;
-        printf("Decoded an int64_out: %lld\n", int64_out);
-        break;
-      case 0xc0:
-        buffer++;
-        printf("Decoded a null\n");
-        break;
-      case 0xc3:
-        buffer++;
-        printf("Decoded a true\n");
-        break;
-      case 0xc2:
-        buffer++;
-        printf("Decoded a false\n");
-        break;
-      case 0xca:
-        msgpacker__decode_float(buffer, len_left, &float_out);
-        len_left-=4;
-        printf("Decoded a float: %f\n", float_out);
-        break;
-      case 0xcb:
-        msgpacker__decode_double(buffer, len_left, &double_out);
-        len_left-=8;
-        printf("Decoded a double: %f\n", double_out);
-        break;
-      case 0xda:
-        msgpacker__decode_uint16(buffer, len_left, &uint16_out);
-        len_left-=2;
-        buffer+=2;
-
-        buffer_out = malloc(uint16_out);
-        msgpacker__decode_raw16(buffer, len_left, buffer_out, uint16_out);
-        len_left-=uint16_out;
-
-        printf("Decoded a raw16 %d byte(s) long: %.*s\n", uint16_out, uint16_out, buffer_out);
-        free(buffer_out);
-        break;
-      case 0xdb:
-        msgpacker__decode_uint32(buffer, len_left, &uint32_out);
-        len_left-=4;
-        buffer+=4;
-
-        buffer_out = malloc(uint32_out);
-        msgpacker__decode_raw32(buffer, len_left, buffer_out, uint32_out);
-        len_left-=uint32_out;
-
-        printf("Decoded a raw32 %d byte(s) long: %.*s\n", uint32_out, uint32_out, buffer_out);
-        free(buffer_out);
-        break;
-      case 0xdc:
-        msgpacker__decode_uint16(buffer, len_left, &uint16_out);
-        len_left-=2;
-        buffer+=2;
-
-        /* TODO: loop over `uint16_out` times */
-        len_left = 0;
-
-        printf("Decoded an array16 %d item(s) long\n", uint16_out);
-        break;
-      case 0xdd:
-        msgpacker__decode_uint32(buffer, len_left, &uint32_out);
-        len_left-=2;
-        buffer+=2;
-
-        /* TODO: loop over `uint32_out` times */
-        len_left = 0;
-
-        printf("Decoded an array32 %d item(s) long\n", uint32_out);
-        break;
-      case 0xde:
-        msgpacker__decode_uint16(buffer, len_left, &uint16_out);
-        len_left-=2;
-        buffer+=2;
-
-        /* TODO: loop over `uint16_out` times */
-        len_left = 0;
-
-        printf("Decoded an map16 %d item(s) long\n", uint16_out);
-        break;
-      case 0xdf:
-        msgpacker__decode_uint32(buffer, len_left, &uint32_out);
-        len_left-=2;
-        buffer+=2;
-
-        /* TODO: loop over `uint32_out` times */
-        len_left = 0;
-
-        printf("Decoded an map32 %d item(s) long\n", uint32_out);
-        break;
-      default:
-        if ((leader & 0x80) == 0x0) {
-          printf("Decoded a positive fixnum: %d\n", leader);
-        } else if ((leader & 0xe0) == 0xe0) {
-          printf("Decoded a negative fixnum: %d\n", (int8_t)leader);
-        } else if ((leader & 0xa0) == 0xa0) {
-          int8_out = leader & (~0xa0);
-
-          if (len_left >= (size_t)int8_out) {
-            buffer_out = malloc(int8_out);
-            memcpy(buffer_out, buffer, int8_out);
-            printf("Decoded a fixed raw %d bytes long: %.*s\n", int8_out, int8_out, buffer_out);
-            len_left-=int8_out;
-            buffer+=int8_out;
-            free(buffer_out);
-          }
-        } else if ((leader & 0x90) == 0x90) {
-          int8_out = leader & (~0x90);
-
-          printf("Decoded a fixed array %d items long\n", int8_out);
-          /* TODO: loop over `int8_out` times */
-          len_left = 0;
-        } else if ((leader & 0x80) == 0x80) {
-          int8_out = leader & (~0x80);
-
-          printf("Decoded a fixed map %d items long\n", int8_out);
-          /* TODO: loop over `int8_out` times */
-          len_left = 0;
-        } else {
-          printf("Unknown leader type of 0x%x\n", leader);
-        }
-    }
-  }
-}
-
-int main() {
-  // unsigned char buffer[2] = "\xCC\x80";
-  // unsigned char buffer[3] = "\xCD\x04\xD2";
-  // unsigned char buffer[5] = "\xCE\x00\x01\xE2@";
-  // unsigned char buffer[9] = "\xCF\x00\x00\x00\x01\x00\x00\x00\x00";
-  // unsigned char buffer[2] = "\xD0\x81";
-  // unsigned char buffer[3] = "\xD1\xFA\xF7";
-  // unsigned char buffer[5] = "\xD2\xFF\xFE\x1D\xC0";
-  // unsigned char buffer[9] = "\xD3\xFF\xFF\xFF\xFF\x00\x00\x00\x00";
-  // unsigned char buffer[1] = "\xC0";
-  // unsigned char buffer[1] = "\xC3";
-  // unsigned char buffer[1] = "\xC2";
-  // unsigned char buffer[5] = "\xCA@\x93J=";
-  // unsigned char buffer[9] = "\xCB@\x93J=p\xA3\xD7\n";
-  // unsigned char buffer[4] = "\xDA\x00\x01\x61";
-  // unsigned char buffer[6] = "\xDB\x00\x00\x00\x01\x61";
-  // unsigned char buffer[5] = "\xDC\x00\x01\241a";
-  // unsigned char buffer[7] = "\xDD\x00\x00\x00\x01\241a";
-  // unsigned char buffer[7] = "\xDE\x00\x01\241a\241a";
-  // unsigned char buffer[2] = "\241\x61";
-  // unsigned char buffer[2] = "\x91\x00";
-  unsigned char buffer[4] = "\x81\224\x61\x61";
+int main()
+{
+  // unsigned char buffer[] = "\xCC\x80";
+  // unsigned char buffer[] = "\xCD\x04\xD2";
+  // unsigned char buffer[] = "\xCE\x00\x01\xE2@";
+  // unsigned char buffer[] = "\xCF\x00\x00\x00\x01\x00\x00\x00\x00";
+  // unsigned char buffer[] = "\xD0\x81";
+  // unsigned char buffer[] = "\xD1\xFA\xF7";
+  // unsigned char buffer[] = "\xD2\xFF\xFE\x1D\xC0";
+  // unsigned char buffer[] = "\xD3\xFF\xFF\xFF\xFF\x00\x00\x00\x00";
+  // unsigned char buffer[] = "\xC0";
+  // unsigned char buffer[] = "\xC3";
+  // unsigned char buffer[] = "\xC2";
+  // unsigned char buffer[] = "\xCA@\x93J=";
+  // unsigned char buffer[] = "\xCB@\x93J=p\xA3\xD7\n";
+  // unsigned char buffer[] = "\xDA\x00\x01\x61";
+  // unsigned char buffer[] = "\xDB\x00\x00\x00\x01\x61";
+  // unsigned char buffer[] = "\xDC\x00\x01\241a";
+  // unsigned char buffer[] = "\xDD\x00\x00\x00\x01\241a";
+  // unsigned char buffer[] = "\xDE\x00\x01\241a\241a";
+  // unsigned char buffer[] = "\241\x61";
+  // unsigned char buffer[] = "\x91\x00";
+  unsigned char buffer[] = "\x81\224\x61\x61";
 
   size_t len;
 
