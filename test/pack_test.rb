a7f388e138084e7ca21bdeaa5bef95151cb795ee
Brian Lopez <seniorlopez@gmail.com>
2012-10-08 10:29:06 -0700

    split out tests

---
+++ test/pack_test.rb
@@ -0,0 +1,61 @@
+# encoding: utf-8
+require File.expand_path('../setup', __FILE__)
+
+require 'mochilo'
+require 'stringio'
+
+class MochiloPackTest < MiniTest::Unit::TestCase
+
+  OBJECTS = [
+    {"hello" => "world"},
+    12345,
+    -12345,
+    "hey this is a test",
+    0.231
+  ]
+
+  def bytes(a)
+    a.bytes.to_a.map { |b| "0x" + b.to_s(16) }.join(' ')
+  end
+
+  def all_objects
+    objects = OBJECTS.map{ |obj| Mochilo.pack(obj) }.join
+    if objects.respond_to?(:encoding)
+      objects.force_encoding('binary')
+    end
+    objects
+  end
+
+  def test_simple_pack
+    OBJECTS.each do |obj|
+      a = Mochilo.pack(obj)
+      b = Mochilo.unpack(a)
+      assert_equal obj, b
+    end
+  end
+
+  def test_stream_pack
+    stream = StringIO.new
+
+    packer = Mochilo::Packer.new(stream)
+    OBJECTS.each { |obj| packer << obj }
+    packer.flush
+
+    stream.rewind
+    serialized = stream.read
+    if serialized.respond_to?(:encoding)
+      serialized.force_encoding('binary')
+    end
+    assert_equal all_objects, serialized
+  end
+
+  def test_block_pack
+    buffer = ""
+
+    packer = Mochilo::Packer.new { |bytes| buffer << bytes }
+    OBJECTS.each { |obj| packer << obj }
+    packer.flush
+
+    assert_equal all_objects, buffer
+  end
+end
---
+++ test/xplatform_test.rb
@@ -0,0 +1,33 @@
+# encoding: utf-8
+require File.expand_path('../setup', __FILE__)
+
+require 'mochilo'
+
+class CrossPlatformTest < MiniTest::Unit::TestCase
+  ROOT_DIR = File.dirname(File.dirname(__FILE__))
+
+  CASES = [
+    false, true, nil,
+    0,0,0,0,0,0,0,0,0,
+    -1,-1,-1,-1,-1,
+    127,127,255,65535,4294967295,
+    -32,-32,-128,-32768,-2147483648,
+    0.0,-0.0,1.0,-1.0,
+    "a","a","a",
+    "","","",
+    [0],[0],[0],
+    [],[],[],
+    {},{},{},
+    {"a" => 97}, {"a" => 97}, {"a" => 97},
+    [[]], [["a"]]
+  ]
+
+  def test_cross_plat
+    unpacked = []
+    stream = File.new("#{ROOT_DIR}/test/assets/cases.mpac")
+    unpacker = Mochilo::Unpacker.new(stream)
+    unpacker.each { |obj| unpacked << obj }
+
+    assert_equal CASES, unpacked
+  end
+end
