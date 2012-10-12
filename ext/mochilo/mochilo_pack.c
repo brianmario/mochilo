4d7a46d03012f2dd7bade79b55c0682b39455c43
Brian Lopez <seniorlopez@gmail.com>
2012-10-12 15:03:46 -0700

    switch back to using BUILTIN_TYPE

--- ext/mochilo/mochilo_pack.c
+++ ext/mochilo/mochilo_pack.c
@@ -200,12 +200,18 @@ void mochilo_pack_one(mochilo_buf *buf, VALUE rb_object)
 	}
 	else if (FIXNUM_P(rb_object)) {
 		mochilo_pack_fixnum(buf, rb_object);
+	} else if(SYMBOL_P(rb_object)) {
+		rb_object = rb_funcall(rb_object, rb_intern("to_s"), 0);
+#ifdef HAVE_RUBY_ENCODING_H
+		if (ENCODING_GET(rb_object) != 0)
+			mochilo_pack_str(buf, rb_object);
+		else
+#endif
+			mochilo_pack_bytes(buf, rb_object);
 	}
 
 	else {
-		switch (TYPE(rb_object)) {
-		case T_SYMBOL:
-			rb_object = rb_funcall(rb_object, rb_intern("to_s"), 0);
+		switch (BUILTIN_TYPE(rb_object)) {
 		case T_STRING:
 #ifdef HAVE_RUBY_ENCODING_H
 			if (ENCODING_GET(rb_object) != 0)
@@ -233,7 +239,7 @@ void mochilo_pack_one(mochilo_buf *buf, VALUE rb_object)
 		}
 	}
 #else // RUBINIUS
-	switch (TYPE(rb_object)) {
+	switch (rb_type(rb_object)) {
 		case T_NIL:
 			mochilo_buf_putc(buf, MSGPACK_T_NIL);
 			return;
